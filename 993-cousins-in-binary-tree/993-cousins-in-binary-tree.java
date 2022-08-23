b84dde674132926488f6474c173c950bf2fffac2
RAHUL UDHAYAKUMAR <65881610+RahulDeMaverick@users.noreply.github.com>
2022-08-22 22:08:45 -0400

    Time: 2 ms (20.20%), Space: 41.8 MB (61.64%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -26,19 +26,21 @@ class Solution {
                 int size = q.size();
                 for(int i =0;i<size;i++){
                     TreeNode curr = q.poll();
-                    
+                    // setting the flag if the elemnts are found
                     if(curr.val == x) x_found = true;
                      if(curr.val == y) y_found = true;
-                    
+                    // before adding to the queue check if the right and left nodes are from same parents
+                    //if from same parent return false
                     if(curr.left !=null && curr.right != null){
                     if(curr.left.val == x && curr.right.val == y ) return false;
                     if(curr.right.val == x && curr.left.val == y ) return false;
                         }
+                    // add left and right node to the queue
                     if(curr.left !=null) q.add(curr.left);
                     if(curr.right !=null) q.add(curr.right);
                 }
                 if(x_found && y_found) return true;
-                if(x_found || y_found) return false;
+             //   if(x_found || y_found) return false;
             }
             
             return false;
