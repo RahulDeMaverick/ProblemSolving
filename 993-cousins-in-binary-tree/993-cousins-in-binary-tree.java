ee946428172248531da760cd2303400b73591851
RAHUL UDHAYAKUMAR <65881610+RahulDeMaverick@users.noreply.github.com>
2022-08-22 22:05:42 -0400

    Time: 0 ms (100.00%), Space: 40.2 MB (91.62%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -13,12 +13,43 @@
  *     }
  * }
  */
+
+// BFS solution
+
+class Solution {
+        public boolean isCousins(TreeNode root, int x, int y) {
+            Queue<TreeNode> q = new LinkedList<>();
+            q.add(root);
+            while(!q.isEmpty()){
+                boolean x_found = false;
+                boolean y_found = false;
+                int size = q.size();
+                for(int i =0;i<size;i++){
+                    TreeNode curr = q.poll();
+                    
+                    if(curr.val == x) x_found = true;
+                     if(curr.val == y) y_found = true;
+                    
+                    if(curr.left !=null && curr.right != null){
+                    if(curr.left.val == x && curr.right.val == y ) return false;
+                    if(curr.right.val == x && curr.left.val == y ) return false;
+                        }
+                    if(curr.left !=null) q.add(curr.left);
+                    if(curr.right !=null) q.add(curr.right);
+                }
+                if(x_found && y_found) return true;
+                if(x_found || y_found) return false;
+            }
+            
+            return false;
+        }
+}
 //Dfs solution
 //nodes should be of same level but different parents
 
 //TC: o(n)
 //SC: o(h) height of the tree
-class Solution {
+/*class Solution {
     TreeNode x_parent;
     TreeNode y_parent;
     int x_depth;
@@ -52,3 +83,4 @@ class Solution {
         }
     }
 }
+*/
