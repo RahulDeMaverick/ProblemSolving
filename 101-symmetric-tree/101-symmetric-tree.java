bd94968ba7ef023df3b28c2a281da3809ec63e82
RAHUL UDHAYAKUMAR <65881610+RahulDeMaverick@users.noreply.github.com>
2022-08-23 12:10:41 -0400

    Time: 1 ms (64.31%), Space: 42.5 MB (40.71%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -27,24 +27,33 @@ class Solution {
 
         //base
         if(left == null && right == null) return;
-
+        // if left is null or right is null then return false
+        
         if(left == null || right == null)
         {
             flag= false;
             return;
         }   
         
+        // if left.val not equal to right.val then symentic tree is breached
         if(left.val != right.val)
         {
             flag= false;
             return;
         }
+        
         if(flag)
+        // go throug left.left and right.right
+        // left most left and right most right values are iterated recusively
         dfs(left.left, right.right);
         //stack.pop();
+        
+        
         if(flag)
+            // go throug left.right and right.right
+            // left's right and right's left values are iterated recusively
         dfs(left.right,right.left);
-              //stack.pop();
+        //stack.pop();
           
     }
     
