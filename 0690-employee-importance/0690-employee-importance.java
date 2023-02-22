22e513b5a5fdc2fef289656b7222213442e84243
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-02-22 16:26:23 -0500

    Time: 5 ms (67.37%), Space: 61.3 MB (5.20%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.java
@@ -0,0 +1,29 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution {
+    HashMap<Integer,Employee> map;
+    int result;
+    public int getImportance(List<Employee> employees, int id) {
+        map = new HashMap<>();
+        for(Employee e : employees){
+            map.put(e.id,e);
+        }
+        dfs(id);
+        return result;        
+    }
+    
+    private void dfs(int id){
+        Employee e = map.get(id);
+        result += e.importance;
+        for (int subID : e.subordinates){
+            dfs(subID);
+        }
+    }
+}
