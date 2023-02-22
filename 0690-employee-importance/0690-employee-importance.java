eee3664528217606fc4ec2fbb285fdefe445a03f
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-02-22 16:41:43 -0500

    Time: 5 ms (67.37%), Space: 61 MB (5.20%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.java
+++ 0690-employee-importance/0690-employee-importance.java
@@ -7,22 +7,35 @@ class Employee {
 };
 */
 
+//TC: O(V+E) always for graphs
+// here it will be mostly o(N)
+/*
 class Solution {
     HashMap<Integer,Employee> map;
+    //BFS solution
     
     public int getImportance(List<Employee> employees, int id) {
         int result = 0;
         map = new HashMap<>();
+        //adding Employee object to the queue
         Queue<Employee> q = new LinkedList<>();
+        //iterating over all the employees
         for(Employee e : employees){
+            //maintaining employee map with emp id , empolyee object
             map.put(e.id,e);
         }
+        //add the employee id to the queue
         q.add(map.get(id));
         while(!q.isEmpty()){
+            //poll the employee object
             Employee e = q.poll();
+            //add the importance
             result += e.importance;
+            //iterate over the subordinates
             for(int subID : e.subordinates){
+                //add the sub employee from the map
                 Employee subemp = map.get(subID);
+                //add the sub emp to the queue
                 q.add(subemp);
             }
         }
@@ -31,9 +44,9 @@ class Solution {
 
     }
 }
-
+*/
 //DFS Solution
-/*
+
 class Solution {
     HashMap<Integer,Employee> map;
     int result;
@@ -47,11 +60,14 @@ class Solution {
     }
     
     private void dfs(int id){
+    //get the employee object
         Employee e = map.get(id);
+        //calculate importance result
         result += e.importance;
+        //iterrate over the subordinates
         for (int subID : e.subordinates){
+        //recusively call all the sub ordinates
             dfs(subID);
         }
     }
 }
-*/
