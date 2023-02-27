02eae28e18842124b5bdf7c4a70ed261ec2e5681
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-02-26 23:03:14 -0500

    Time: 13 ms (20.79%), Space: 42.9 MB (28.78%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
         
+        //create frequecy map
        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i : nums)
           {
             map.put(i, map.getOrDefault(i,0)+1);
 
         }
+        
+        //if map.get(i) returns only one then return
           for(int i :nums){
               if(map.get(i) ==1)
               {
