c9b7737a6e0ba20a49613acf3ee1be7ae0ec6d9d
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-03-11 14:04:38 -0500

    Time: 9 ms (42.53%), Space: 47.2 MB (61.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,10 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        
+        //First sot the intervals using comparator
         Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
-        List<int[]> result = new ArrayList<>();   
+        
+        // initialize int [] array list 
+        List<int[]> result = new ArrayList<>();  
+        // declare start and end pointers
         int start = intervals[0][0], end = intervals[0][1];
+        //iterate over the intervals
         for(int[] i: intervals){
+            //if my interval[0] is greatter than end
+            //then no overlap
             if(i[0] > end){ //no overlap
+                //create a new int array 
             result.add(new int[] {start,end});
             start = i[0];
             end = i[1];
@@ -14,7 +23,7 @@ class Solution {
             }
         }
         result.add(new int[] {start,end});
-        return result.toArray(new int[result.size()][2]);
+        return result.toArray(new int[result.size()][0]);
         
     }
 }
