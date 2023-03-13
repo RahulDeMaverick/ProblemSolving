65d930d28c886fc175419d2f1f29ae6898e103a8
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-03-13 12:08:20 -0400

    Time: 2 ms (99.00%), Space: 50.8 MB (51.90%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,12 +1,20 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
+        //draw the graph to understand the problem
+        //create a indegrees array
+        //iterate over trust 2d array
+        
         
         int[] indegrees = new int[n];
+        //indegrees array is count of no of in direction and out direction in a graph
         
         for(int[] in: trust){
+            //if a person gives support to other then we should reduce is indegree array
             indegrees[in[0]-1]--;
+            //if a person is getting a support then increases is count by one
             indegrees[in[1]-1]++;
         }
+        //iterate over the indegrees if any of the values is equal to n-1 then return 
         for(int i =0;i< n;i++){
             if(indegrees[i] ==n-1) return i+1;
         }
