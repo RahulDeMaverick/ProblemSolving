c3d0a534c63263cf2c699483c39b5e1f6ef5dd81
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-03-23 11:24:30 -0400

    Time: 6 ms (80.74%), Space: 41.6 MB (77.51%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -4,19 +4,31 @@ class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         if(arr == null || arr.length ==0);
         int n = arr.length;
+        //initate dp array
         int [] dp = new int[n];
+        //dp[0] is arr[0]
         dp[0] = arr[0];
+        //iterate over dp array
         for(int i=1;i<n;i++){
+            //declare the max value with arr[i] to calculate all the other max values
             int max =arr[i];
+            //go over j till less than or equal to k and also check i-j+1 is >=0 to be in bound or else we will get arrayoutofindex
             for(int j=1;j<=k && i-j +1>=0;j++){
+                //calculate the max between the earlier max and the running k partition.
+                //we dont need to calculate the max in k partition just compare with current element with previous max
                 max = Math.max(max,arr[i-j+1]);
+                //once we get max just multiply with j
+                //eg : if max is 15 then and j is 2 or 3 depening upon the partition just multiply the max with j
                  int currPart = max*j;
+                //if i-j is in bound 
                 if(i-j >=0)
-                     currPart = max*j + dp[i-j];
-                    
-                dp[i] = Math.max(dp[i],currPart);
+                    //current part is max *j + dp[i-j]
+                    currPart += dp[i-j]; 
+                //calculate the max between curr dp[i] and curr partion
+                    dp[i] = Math.max(dp[i],currPart);
             }
         }
+        //return last element in the Dp array
         return dp[n-1];
     }
 }
