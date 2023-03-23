14e56b864629d0fbee82ce0db0fa67eb661f7f81
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-03-23 15:32:59 -0400

    Time: 0 ms (100.00%), Space: 39.4 MB (40.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,13 +1,23 @@
+//bottom up dp
+//TC: O(M*N)
+//SC: O(M*N)
+//approch: fill the last row and last column in the dp array as 1
+//there is only one way it can move its either down or right so fill the last row and last column with 1
+//now iterate over n-2/m-2 index of the array and calculate the dp array with adding dp[i+1][j] with dp[i][j+1]
+//like wise calculate all the dp array now dp[0][0] will have all the unique paths
 class Solution {
     public int uniquePaths(int m, int n) {
+        //dp array 
         int[][] dp = new int[m][n];
+        //filling last row
         for(int i=0;i<  n;i++){
             dp[m-1][i] = 1;
         }
+        //filling last column with 1
         for(int i=0;i< m;i++){
             dp[i][n-1] = 1;
         }
-        
+        //filling up dp array
         for(int i=m-2;i>=0;i--){
             for(int j =n-2;j>=0;j--){
                 dp[i][j] = dp[i+1][j]+ dp[i][j+1];
