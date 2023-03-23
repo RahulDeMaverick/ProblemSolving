c50216c986c5a2393618737a3a5ee541eab784b5
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-03-23 15:11:05 -0400

    Time: 0 ms (100.00%), Space: 39 MB (77.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,9 @@
+//TC: O(M*N)
+//SC: O(M*N)
 class Solution {
+    //memoraization technique
+    //call recurssively and save it in to memo array (in a data struture) 
+    //top down dp is nothing calling the recursion but saving the recursion in to a data structure to memorize it and we can use them
     int [][] memo;
     public int uniquePaths(int m, int n) {
         memo = new int[m][n];
@@ -8,13 +13,15 @@ class Solution {
     private int helper(int i, int j, int m, int n){
         //base
         
-        //if(i==m || j==n) return 0;
+        if(i==m || j==n) return 0;
         if(i==m-1 && j==n-1) return 1;
         
         
         //logic
+        //case 1 is moving right and case 2 is moving down.
         int case1=0;
         if(i<m-1){
+            //if i+1 j array is zero then go and calculate else if already calculated use it
             if(memo[i+1][j] ==0)
             {
              memo[i+1][j]  = helper( i+1,  j,  m,  n);
