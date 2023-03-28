f91ace693944aaa04f89b558579bdbb3c818238d
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-03-27 22:31:25 -0400

    Time: 1 ms (55.81%), Space: 42.5 MB (52.82%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -4,10 +4,12 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         result = new ArrayList<>();
         if(nums ==null || nums.length ==0) return result;
-        helper(nums,0,new ArrayList<>());
+        //01 recurstion
+       // helper(nums,0,new ArrayList<>()); 
+         backtrack(nums,0,new ArrayList<>());
         return result;
     }
-    
+ /*   
     private void helper(int[] nums,int i,List<Integer> path){
         //base
         if(i==nums.length){
@@ -27,5 +29,19 @@ class Solution {
         
         //backtrack
         path.remove(path.size()-1);
+    }
+    */
+    
+    private void backtrack(int[] nums,int idx,List<Integer> path){
+        
+        result.add(new ArrayList<>(path));        
+        for(int i =idx;i<nums.length;i++){
+            //action
+            path.add(nums[i]);
+            //recurse
+            backtrack(nums,i+1,path);
+            //backtrack
+            path.remove(path.size()-1);
+        }
     }
 }
