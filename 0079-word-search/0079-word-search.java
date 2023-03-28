c503a6da6f86bddab1376c167e6ee9142d34d104
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-03-27 20:51:36 -0400

    Time: 254 ms (32.27%), Space: 40.9 MB (34.64%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -9,10 +9,13 @@ class Solution {
          n = board[0].length;
 
         dirs = new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-
+        
+        //iterate over the board array to find the first character in the target word.
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
+                //if we found the first character then recusively we will check the neighbours
                 if(word.charAt(0) == board[i][j]){
+                    //if backtrack funtion returns true then we found all the word
                     if(backtrack(board,word,i,j,0)) return true;
                 }
 
@@ -23,16 +26,21 @@ class Solution {
     
     private boolean backtrack(char[][] board, String word, int i, int j, int idx){
         //base
+        //if we index reaches word length then we able to find all the words in the word
         if(idx == word.length()) return true;
+        //boundary check 
         if(i<0 || j<0 || i ==m || j==n || board[i][j]=='#') return false;
         
         //logic
+        //check if board[i][j] is equal to word charat() then we continue the process
         if(board[i][j] == word.charAt(idx)){
-
+            
+            //change the board[i][j] to different character so that we dont calculate the same value when we recursively call
             //action
             board[i][j] = '#';
 
             //recurse
+            //iterate over dirs array and call recusively
             for(int[] dir: dirs){
                 int r = i+dir[0];
                 int c = j+dir[1];
@@ -41,6 +49,8 @@ class Solution {
             }
             
             //backtrack
+            //backtrack and change the action we made
+
             board[i][j] = word.charAt(idx);
         }
         return false;
