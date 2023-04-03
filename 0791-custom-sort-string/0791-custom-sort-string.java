ed46e4e2a1111f77cf24c52c214bde5ade3a3092
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-04-03 17:07:38 -0400

    Time: 2 ms (62.72%), Space: 40.5 MB (62.51%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -6,14 +6,19 @@ class Solution {
             char c = s.charAt(i);
             map.put(c,map.getOrDefault(c,0)+1);
         }
+        //result stringbuilder to append result
         StringBuilder sb = new StringBuilder();
+        //iterate over order
         for(int i=0;i<order.length();i++){
             char c = order.charAt(i);
+            //if the order is present in the frequecy map
             if(map.containsKey(c)){
+                //append that character till the count of the character present in the frequecy map
                 int count = map.get(c);
                 for(int k=0;k<count;k++){
                     sb.append(c);
                 }
+                //after appending remove the character from c
                 map.remove(c);
             }
         }
