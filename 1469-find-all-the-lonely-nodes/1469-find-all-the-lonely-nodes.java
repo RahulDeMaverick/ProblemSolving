47e9ea932dee4b370ab727841642c416ca7ae0f0
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-04-09 20:31:57 -0400

    Time: 45 ms (6.74%), Space: 43.5 MB (21.35%) - LeetHub

---
+++ 1469-find-all-the-lonely-nodes/1469-find-all-the-lonely-nodes.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public List<Integer> getLonelyNodes(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            for(int i=0;i<q.size();i++){
+                TreeNode curr = q.poll();
+                int parent = curr.val;
+                System.out.println(parent);
+
+                if(curr.left == null && curr.right != null) {
+                    result.add(curr.right.val);
+                }
+
+                if(curr.right == null && curr.left != null) {
+                    result.add(curr.left.val);
+                }
+                
+                if(curr.left !=null) q.add(curr.left);
+                if(curr.right !=null) q.add(curr.right);
+                
+            }
+        }
+        return result;
+    }
+}
