80a8c95545d1f8b2281eed9047e2a55ac70c7c37
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-04-18 11:25:57 -0400

    Time: 1 ms (81.07%), Space: 40.5 MB (92.96%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -5,21 +5,25 @@ class Solution {
         int len2 = word2.length();
         int ptr1 =0;
         int ptr2 =0;
-        while(ptr1<len1 && ptr2<len2){
+        while(ptr1<len1 || ptr2<len2){
+            if(ptr1<len1)
+            {
             sb.append(word1.charAt(ptr1));
             ptr1++;
+            }
+            if(ptr2<len2){
             sb.append(word2.charAt(ptr2));
             ptr2++; 
-        }
-        
-        while(ptr1<len1){
-            sb.append(word1.charAt(ptr1));
-            ptr1++;
-        }
-        while(ptr2<len2){
-            sb.append(word2.charAt(ptr2));
-            ptr2++;
-        }
+            }
+        }  
+        // while(ptr1<len1){
+        //     sb.append(word1.charAt(ptr1));
+        //     ptr1++;
+        // }
+        // while(ptr2<len2){
+        //     sb.append(word2.charAt(ptr2));
+        //     ptr2++;
+        // }
         return sb.toString();
     }
 }
