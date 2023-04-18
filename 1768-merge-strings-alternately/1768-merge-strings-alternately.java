65b0a0141170aa12e869c1ee888d0f6a271e0f76
Rahul Udhatakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-04-18 11:22:14 -0400

    Time: 1 ms (81.07%), Space: 40.4 MB (92.96%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuffer sb = new StringBuffer();
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int ptr1 =0;
+        int ptr2 =0;
+        while(ptr1<len1 && ptr2<len2){
+            sb.append(word1.charAt(ptr1));
+            ptr1++;
+            sb.append(word2.charAt(ptr2));
+            ptr2++; 
+        }
+        
+        while(ptr1<len1){
+            sb.append(word1.charAt(ptr1));
+            ptr1++;
+        }
+        while(ptr2<len2){
+            sb.append(word2.charAt(ptr2));
+            ptr2++;
+        }
+        return sb.toString();
+    }
+}
