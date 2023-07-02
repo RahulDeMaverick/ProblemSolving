179b380207099e482ba8d30b665ba5725bbac6c3
Rahul Udhayakumar <65881610+RahulDeMaverick@users.noreply.github.com>
2023-07-02 17:31:21 -0400

    Time: 2 ms (99.81%), Space: 43.6 MB (51.98%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,22 +1,29 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
         
-        if(nums == null || nums.length == 0) return 0;
-        int max = nums[0];
-        for(int n : nums){
-            max = Math.max(n,max);
+        if(nums == null || nums.length ==0) return 0;   
+        int n = nums.length;
+        if(n==1) return nums[0];
+        int max =nums[0];
+        
+        for(int num: nums){
+            max = Math.max(max,num);
         }
         int[] arr = new int[max+1];
-        for(int n : nums){
-            arr[n] += n;
+        for(int num:nums){
+            arr[num] += num;
         }
-        int prev =0;
-        int curr = arr[1];
+        int[] dp = new int[arr.length];
+
+        dp[0] = arr[0];
+
+        dp[1] = Math.max(arr[0],arr[1]);
+
         for(int i=2;i<arr.length;i++){
-            int temp = curr;
-            curr = Math.max(curr, prev + arr[i]);
-            prev = temp;
-    }
-        return curr;
+            dp[i] = Math.max(dp[i-1],arr[i]+ dp[i-2]);
+
+            
+        }
+        return dp[arr.length-1];
 }
 }
